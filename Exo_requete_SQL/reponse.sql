4b6c8c11949a435ce46da278835c8f1d59be068f
Loïc Tauveron <loic.tauveron62@gmail.com>
2020-04-03 10:45:28 +0200

    Modification requete insert into

--- Exo_requete_SQL/reponse.sql
+++ Exo_requete_SQL/reponse.sql
@@ -13,22 +13,38 @@ AND brands.brand_name = 'Haro'
 
 -- Question 2
 
-Create table tva
-(
-	id_tva INT PRIMARY KEY NOT NULL,
-	valeur_tva INT NOT NULL,
-),
+-- Rémi
 
-SELECT prod.product_name AS Nom,
-       prod.model_year AS Année,
-       prod.list_price AS Prix HT,
-       tva.valeur_tva,
-       
+SELECT prod.name AS Nom, 
+       prod.model_year AS Année, 
+    prod.price AS PrixHT, 
+       prod.price / (100 * 20) AS Montant20prcent, 
+       ROUND(prod.price + (prod.price / (100 * 20)), 2) AS prixTTC,   
+       cat.nameCat AS typeVélo,  
+       brands.brand_name AS Marque
+FROM products AS prod, categories AS cat, brands
+WHERE cat.idCat = 5
+AND brands.brand_id = 2
+
+-- Edwina
+
+
+SELECT  products.name, 
+        products.model_year, 
+        categories.nameCat, 
+        brands.brand_name, 
+        products.price AS PrixHT, 
+        CAST(products.price*0.2 as DECIMAL(9,2)) AS TVA, 
+        CAST(products.price*1.2 as DECIMAL(9,2)) AS PrixTTC
+FROM products
+INNER JOIN categories ON products.category_id = categories.idCat
+INNER JOIN brands ON products.brand_id = brands.brand_id
     
 
 -- Question 3
 
-SELECT prod.name AS Nom
+SELECT prod.name AS Nom,
+	prod.price AS Prix
 FROM products AS prod
 WHERE prod.price <= 1500
 AND prod.price >= 500
@@ -43,10 +59,10 @@ WHERE prod.name LIKE 'H%'
 --Question 5 
 
 SELECT prod.name AS Nom,
-	prod.model_year AS Année,
+	prod.created AS Année,
         prod.price as Prix
 FROM products AS prod
-WHERE prod.name LIKE '%Ice%'
+WHERE prod.name LIKE '%Lce%'
 
 -- Question 6
 
@@ -63,7 +79,7 @@ where products.category_id = 6
 
 UPDATE products
 SET price = 1499
-WHERE id = 9
+WHERE products.id = 9
 
 -- Question 9 
 
@@ -71,5 +87,5 @@ INSERT INTO categories VALUES (8,'Roller skates')
 
 -- Question 10
 
-INSERT INTO `products`(id,name,brand_id,category_id,model_year,price) VALUES (15, 'roller skate cool', 2, 8, 2020, 258)
+INSERT INTO `products`(name,brand_id,category_id,model_year,price) VALUES ('roller skate cool', 2, 8, 2020, 258)
 
