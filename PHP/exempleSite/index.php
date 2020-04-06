85280e59c8e5653eaf56b85ca675369e5cf7faf0
Loïc Tauveron <loic.tauveron62@gmail.com>
2020-04-06 13:17:24 +0200

    finalisation exercice modele de voiture

---
+++ PHP/exempleSite/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil Mesvoitures</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h1>MesVoitures</h1>
+    <a href="mesVehicules.php">Lien vers la liste de mes vehicules</a>
+</body>
+</html>
---
+++ PHP/exempleSite/mesVehicules.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste de mes vehicules</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<?php
+
+$b = "<br>";
+
+$mesVehicules = array(
+    'Citroen' => 'C4',
+    'Renault' => 'Clio',
+    'Peugeot' => '306',
+    'Nissan' => 'Juke',
+    'Mercedes' => 'BENZ',
+    'Volkswagen' => 'Golf',
+    'Suzuki' => 'Swift',
+    'Audi' => 'TT',
+);
+?>
+    <table>
+        <tr>
+            <th>Marques</th>
+            <th>Modeles</th>
+        </tr>
+<?php
+foreach($mesVehicules as $marque => $modele){
+?>
+        <tr>
+            <td><?php echo $marque;?></td>
+            <td><?php echo $modele;?></td>
+        </tr>
+<?php
+}
+?>
+    </table>
+</body>
+</html>
