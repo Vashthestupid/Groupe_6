7ffb73231742107689bb3bd6e74e2590c02ee884
Loïc Tauveron <loic.tauveron62@gmail.com>
2020-04-27 11:09:31 +0200

    modification de la couleur des boutons sur la page connexion.html

--- projet_stage/public/js/main.js
+++ projet_stage/public/js/main.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
     $('#btnInscription').click(function(){
         $('#inscription').show();
         $('#connexion').hide();
+        $('#btnInscription').removeClass('btn-secondary').addClass('btn-primary');
+        $('#btnConnexion').removeClass('btn-primary').addClass('btn-secondary');
     });
 
    $('#btnConnexion').click(function(){
         $('#connexion').show();
         $('#inscription').hide();
+        $('#btnConnexion').removeClass('btn-secondary').addClass('btn-primary');
+        $('#btnInscription').removeClass('btn-primary').addClass('btn-secondary');
    });
 })
