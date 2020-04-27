6dd4812a468c809547e1cd0f6b5078e28a4d4715
Loïc Tauveron <loic.tauveron62@gmail.com>
2020-04-27 13:21:25 +0200

    création du lien gererProduits sur toutes les pages puis création de cette même page

--- projet_stage/public/js/main.js
+++ projet_stage/public/js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 
     $('#btnInscription').click(function(){
-        $('#inscription').show();
-        $('#connexion').hide();
         $('#btnInscription').removeClass('btn-secondary').addClass('btn-primary');
         $('#btnConnexion').removeClass('btn-primary').addClass('btn-secondary');
+        $('#inscription').show();
+        $('#connexion').hide();
+        
     });
 
    $('#btnConnexion').click(function(){
-        $('#connexion').show();
-        $('#inscription').hide();
         $('#btnConnexion').removeClass('btn-secondary').addClass('btn-primary');
         $('#btnInscription').removeClass('btn-primary').addClass('btn-secondary');
+        $('#connexion').show();
+        $('#inscription').hide();
+        
    });
 })
